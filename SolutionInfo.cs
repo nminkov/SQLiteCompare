0d1fb9ef1689df7aa0567f55a2b578a2ff3826d0
datadiode <j.tucht@arcor.de>
2020-08-08 20:35:34 +0200

    Apply Zoltu.Versioning

--- SolutionInfo.cs
+++ SolutionInfo.cs
@@ -21,13 +21,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("BD655607-78A1-471B-A296-BB55E33D878C")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-[assembly: AssemblyVersion("3.0.*")]
-[assembly: AssemblyFileVersion("3.0.*")]
