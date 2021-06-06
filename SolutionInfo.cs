82402fe8406cfd4eb3e572a76ef60bd4abef4142
datadiode <datadiode@users.noreply.github.com>
2021-06-06 11:11:49 +0200

    Convert .csproj to Microsoft.NET.Sdk

--- SolutionInfo.cs
+++ SolutionInfo.cs
@@ -1,23 +1,7 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyProduct("SQLite Compare")]
-[assembly: AssemblyTitle("SQLite Compare")]
 [assembly: AssemblyDescription("Utility for comparing two SQLite database files for both structure and data.")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("SCHMID Group | Gebr. SCHMID GmbH")]
 [assembly: AssemblyCopyright("Copyright © 2016 - SCHMID Group | Gebr. SCHMID GmbH")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("BD655607-78A1-471B-A296-BB55E33D878C")]
